6d007c180075e81adbfd08b463261b24d3bb35e7
Tusharnp16 <tusharnp16@gmail.com>
2025-07-06 10:29:12 +0530

    gearNest first commit

---
+++ gearnest/src/main/java/com/example/gearnest/GearnestApplication.java
@@ -0,0 +1,14 @@
+package com.example.gearnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearnestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(GearnestApplication.class, args);
+	}
+
+}
---
+++ gearnest/src/test/java/com/example/gearnest/GearnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gearnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
