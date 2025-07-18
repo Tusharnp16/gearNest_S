1a03529aa5884080da09e81c15fa5a720c0d4033
Your NameNilesh-Sonavane <you@example.comnrsonawane2003@gmail.com>
2025-07-18 23:25:27 +0530

    add features of reset password with role

---
+++ gearnest/src/main/java/com/example/gearnest/model/PasswordResetToken.java
@@ -0,0 +1,104 @@
+package com.example.gearnest.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String role;
+    private String token;
+    private LocalDateTime expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+
+    @OneToOne
+    @JoinColumn(name = "garage_id")
+    private Garage garage;
+
+    public boolean isExpired() {
+        return expiryDate.isBefore(LocalDateTime.now());
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public Garage getGarage() {
+        return garage;
+    }
+
+    public void setGarage(Garage garage) {
+        this.garage = garage;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ gearnest/src/main/java/com/example/gearnest/repository/GarageRepository.java
@@ -0,0 +1,9 @@
+package com.example.gearnest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.gearnest.model.Garage;
+
+public interface GarageRepository extends JpaRepository<Garage, Long> {
+    Garage findByEmail(String email);
+}
